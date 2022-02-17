2e01a3e0cb90e2489ba9bc7aeb41681548eb7830
Gaby <gaby.es.iaa@gmail.com>
2022-02-17 15:49:58 -0500

    Move random quote state to main component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 import './scss/App.scss';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
@@ -7,13 +8,69 @@ import Home from './pages/Home';
 import Quote from './pages/Quote';
 
 function App() {
+  const [quote, setQuote] = useState({
+    message: 'Mathematic is about numbers, equations, computations, or algorithms: It is about understanding.',
+    author: 'William Paul Thurston, American mathematician',
+  });
+
+  const quotes = [
+    {
+      message: 'Do not worry about your difficulties in mathematics. I can assure you mine are still greater.',
+      author: 'Albert Einstein, German theoretical physicist',
+    },
+    {
+      message: 'The study of mathematics, like the Nile, begins in minuteness but ends in magnificence.',
+      author: 'Charles Caleb Colton, English cleric, writer and collector',
+    },
+    {
+      message: 'Without mathematics, there’s nothing you can do. Everything around you is mathematics. Everything around you is numbers.',
+      author: 'Shakuntala Devi, Indian writer and mental calculator',
+    },
+    {
+      message: 'In mathematics the art of proposing a question must be held of higher value than solving it.',
+      author: 'Georg Ferdinand Ludwig Philipp Cantor',
+    },
+    {
+      message: 'Mathematic is about numbers, equations, computations, or algorithms: It is about understanding.',
+      author: 'William Paul Thurston, American mathematician',
+    },
+    {
+      message: 'Mathematics is the most beautiful and most powerful creation of the human spirit.',
+      author: 'Stefan Banach, Polish mathematician',
+    },
+    {
+      message: 'As far as the laws of mathematics refer to reality, they are not certain, and as far as they are certain, they do not refer to reality.',
+      author: 'Albert Einstein, German theoretical physicist',
+    },
+    {
+      message: 'Mathematics knows no races or geographic boundaries; for mathematics, the cultural world is one country.',
+      author: 'David Hilbert, German mathematician',
+    },
+    {
+      message: '‘Obvious’ is the most dangerous word in mathematics.',
+      author: 'Eric Temple Bell, Scottish mathematician',
+    },
+    {
+      message: 'The essence of math is not to make simple things complicated, but to make complicated things simple.',
+      author: 'Stan Gudder, American mathematician',
+    },
+  ];
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const newIndex = Math.floor(Math.random() * quotes.length);
+      setQuote(quotes[newIndex]);
+    }, 15000);
+    return () => clearInterval(interval);
+  });
+
   return (
     <div className="App">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/calculator" element={<Calculator />} />
-        <Route path="/quote" element={<Quote />} />
+        <Route path="/quote" element={<Quote quote={quote} />} />
         <Route path="*" element={<Home />} />
       </Routes>
       <Footer />
--- src/pages/Quote.js
+++ src/pages/Quote.js
@@ -1,56 +1,22 @@
-import { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 
-const Quote = () => {
-  const [quote, setQuote] = useState({
-    message: 'Mathematic is about numbers, equations, computations, or algorithms: It is about understanding.',
-    author: 'William Paul Thurston',
-  });
+const Quote = ({ quote }) => (
+  <main className="page__quote">
+    <p className="quote__message">
+      &quot;
+      { quote.message }
+      &quot;
+    </p>
+    <p className="quote__author">
+      &mdash;
+      <strong>{ quote.author }</strong>
+      &mdash;
+    </p>
+  </main>
+);
 
-  const quotes = [
-    {
-      message: 'Do not worry about your difficulties in mathematics. I can assure you mine are still greater.',
-      author: 'Albert Einstein',
-    },
-    {
-      message: 'The study of mathematics, like the Nile, begins in minuteness but ends in magnificence.',
-      author: 'Charles Caleb Colton',
-    },
-    {
-      message: 'Without mathematics, there’s nothing you can do. Everything around you is mathematics. Everything around you is numbers.',
-      author: 'Shakuntala Devi',
-    },
-    {
-      message: 'In mathematics the art of proposing a question must be held of higher value than solving it.',
-      author: 'Georg Ferdinand Ludwig Philipp Cantor',
-    },
-    {
-      message: 'Mathematic is about numbers, equations, computations, or algorithms: It is about understanding.',
-      author: 'William Paul Thurston',
-    },
-  ];
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      const newIndex = Math.floor(Math.random() * quotes.length);
-      setQuote(quotes[newIndex]);
-    }, 30000);
-    return () => clearInterval(interval);
-  });
-
-  return (
-    <main className="page__quote">
-      <p className="quote__message">
-        &quot;
-        { quote.message }
-        &quot;
-      </p>
-      <p className="quote__author">
-        &mdash;
-        <strong>{ quote.author }</strong>
-        &mdash;
-      </p>
-    </main>
-  );
+Quote.propTypes = {
+  quote: PropTypes.objectOf(PropTypes.string.isRequired).isRequired,
 };
 
 export default Quote;
