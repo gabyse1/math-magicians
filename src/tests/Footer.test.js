f713b25af04a6fb0fbd4c16db40cfd62e72bbda0
Gaby <gaby.es.iaa@gmail.com>
2022-02-17 15:48:10 -0500

    Delete unused files

--- src/tests/Footer.test.js
+++
@@ -1,9 +0,0 @@
-import renderer from 'react-test-renderer';
-import Footer from '../components/Footer';
-
-describe('<Footer /> component', () => {
-  it('Component render correctly', () => {
-    const tree = renderer.create(<Footer />).toJSON();
-    expect(tree).toMatchSnapshot();
-  });
-});
--- src/tests/Home.test.js
+++
@@ -1,9 +0,0 @@
-import renderer from 'react-test-renderer';
-import Home from '../pages/Home';
-
-describe('<Home /> component', () => {
-  it('Component render correctly', () => {
-    const tree = renderer.create(<Home />).toJSON();
-    expect(tree).toMatchSnapshot();
-  });
-});
